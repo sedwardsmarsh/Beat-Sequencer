5b4d910b855beb10ad00f8cfac9b38864d088a55
Sam Edwards-Marsh <sam.david.marsh@gmail.com>
2025-10-22 11:34:04 -0700

    Initial Commit

---
+++ Beat-Sequencer/Beat_SequencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beat_SequencerApp.swift
+//  Beat-Sequencer
+//
+//  Created by Sam Edwards-Marsh on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Beat_SequencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beat-SequencerTests/Beat_SequencerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Beat_SequencerTests.swift
+//  Beat-SequencerTests
+//
+//  Created by Sam Edwards-Marsh on 10/22/25.
+//
+
+import Testing
+@testable import Beat_Sequencer
+
+struct Beat_SequencerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
